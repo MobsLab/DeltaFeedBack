f8327389719b6241931f3d0cb9c7fc4faf919843
MobsLab <41677251+MobsLab@users.noreply.github.com>
2018-08-03 19:09:53 +0200

    Delete ComputeMean.m

--- Simu/Compare_offline/compare_detection_downs/functions/ComputeMean.m
+++
@@ -1,5 +0,0 @@
-function mean_signal = ComputeMean (signal2mean,IndexArray,DetectionMatrix)
-
-mean_signal = signal2mean(IndexArray)';
-mean_signal = reshape(mean_signal, [200,size(DetectionMatrix,1)])';
-mean_signal = mean(mean_signal,1);
